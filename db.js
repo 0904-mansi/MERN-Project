1ff7c42a078ae4887779c5c0fe8aa209ded22227
Mansi Mishra <mansimishra510@gmail.com>
2022-04-15 17:08:20 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+var mongoURL = 'mongodb+srv://MansiMishra:mansi0904@cluster0.taqhx.mongodb.net/mern-pizza'
+mongoose.connect(mongoURL, {useUnifiedTopology:true , useNewUrlParser:true})
+var db = mongoose.connection
+db.on('connected' , ()=>{
+    console.log('Mongodb connection is successful')
+})
+db.on('error', ()=>{
+    console.log('Mongodb connection failed')
+})
+
+module.exports=mongoose
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongoose = require("mongoose")
+const app = express();
+const Pizza=require("./Models/pizzamodel")
+const db= require('./db.js')
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+    res.send("Server working" + port);
+})
+
+app.get('/getpizzas', (req, res)=>{
+    Pizza.find({}, (err, docs)=>{
+        if(err){
+            console.log(err); 
+        }else{
+           res.send(docs) 
+        }
+    })
+})
+const port = process.env.PORT || 7000;
+app.listen(port, ()=> `server is listening on ${port}`)
